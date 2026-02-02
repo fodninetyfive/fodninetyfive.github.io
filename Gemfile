7b5d725a068d20bda4020c3e06183177f55d2a56
Christian Davis <fireworksordie@gmail.com>
2026-02-02 14:52:50 -0600

    Code cleanup and modernization
    - Extract duplicated tag lookup logic to _includes/tag_lookup.html
    - Fix JS: replace for...in loops with proper iteration, use addEventListener
    - Fix JS: use querySelector with classes instead of fragile array indices
    - Add SCSS color variables for maintainability
    - Migrate Sass @import to @use (fixes deprecation warnings)
    - Fix me/index.md layout reference (page → me)
    - Add Ruby 4.0 compatibility gems (csv, base64, logger)
    - Update .gitignore

--- Gemfile
+++ Gemfile
@@ -2,10 +2,10 @@
 
 source "https://rubygems.org"
 
-# gem "rails"
 gem "jekyll"
-
 gem "webrick", "~> 1.7"
-gem 'csv'
-gem 'base64'
-gem 'jekyll-archives'
+
+# Required for Ruby 3.4+/4.0+ (moved out of standard library)
+gem "csv"
+gem "base64"
+gem "logger"
--- assets/js/001.js
+++ assets/js/001.js
@@ -1,36 +1,38 @@
 // By: h01000110 (hi)
 // github.com/h01000110
 
-var max = document.getElementsByClassName("btn")[1];
-var min = document.getElementsByClassName("btn")[2];
+document.addEventListener('DOMContentLoaded', function() {
+	var max = document.querySelector(".btn_max");
+	var min = document.querySelector(".btn_min");
 
-function maximize () {
-	var post = document.getElementsByClassName("content")[0];
-	var cont = document.getElementsByClassName("post_content")[0];
-	var wid = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName("body")[0].clientWidth;
+	function maximize () {
+		var post = document.querySelector(".content");
+		var cont = document.querySelector(".post_content");
+		var wid = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-	if (wid > 900) {
-		widf = wid * 0.9;
-		post.style.width = widf + "px";
+		if (wid > 900) {
+			var widf = wid * 0.9;
+			post.style.width = widf + "px";
 
-		if (wid < 1400) {
-			cont.style.width = "99%";
-		} else {
-			cont.style.width = "99.4%";
+			if (wid < 1400) {
+				cont.style.width = "99%";
+			} else {
+				cont.style.width = "99.4%";
+			}
 		}
 	}
-}
 
-function minimize () {
-	var post = document.getElementsByClassName("content")[0];
-	var cont = document.getElementsByClassName("post_content")[0];
-	var wid = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName("body")[0].clientWidth;
+	function minimize () {
+		var post = document.querySelector(".content");
+		var cont = document.querySelector(".post_content");
+		var wid = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-	if ( wid > 900 ) {
-		post.style.width = "800px";
-		cont.style.width = "98.5%";
+		if ( wid > 900 ) {
+			post.style.width = "800px";
+			cont.style.width = "98.5%";
+		}
 	}
-}
 
-max.addEventListener('click', maximize, false);
-min.addEventListener('click', minimize, false);
+	if (max) max.addEventListener('click', maximize, false);
+	if (min) min.addEventListener('click', minimize, false);
+});
